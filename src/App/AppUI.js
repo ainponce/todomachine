e1afd17e61d5bf7a7f15cb91c0eb7bf81c4f7515
Ain Moises Ponce <ponce.ain@gmail.com>
2023-04-17 14:18:52 -0300

    primer push

---
+++ src/App/AppUI.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { TodoContext } from '../TodoContext/index.js';
+import { TodoCounter } from '../TodoCounter/TodoCounter.js';
+import { TodoSearch } from '../TodoSearch/TodoSearch.js';
+import { TodoList } from '../TodoList/TodoList.js';
+import { TodoItem } from '../TodoItem/TodoItem.js';
+import { CreateTodoButton } from '../CreateTodoButton/CreateTodoButton.js';
+
+function AppUI() {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+      <TodoContext.Consumer>
+        {({
+          error,
+          loading,
+          searchedTodos,
+          completeTodo,
+          deleteTodo,
+        }) => (
+          <TodoList>
+            {error && <p>Desesperáte, hubo un error...</p>}
+            {loading && <p>Estamos cargando, no desesperes...</p>}
+            {(!loading && !searchedTodos.deleteTodo) && <p>¡Crea tu primer tarea!</p>}
+
+            {searchedTodos.map(todo => (
+              <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}
+              />
+            ))}
+          </TodoList>
+        )}
+      </TodoContext.Consumer>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export { AppUI };
---
+++ src/TodoItem/TodoItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./TodoItem.css";
+import Trash from "../TodoItem/Trash.svg"
+import Check from "../TodoItem/Check.svg"
+
+function TodoItem(props) {
+
+  return (
+    <li className="TodoItem">
+      
+      <span
+        className={`Icon Icon-check ${props.completed && 'imageCheck--active'}`} onClick={props.onComplete}
+      >
+        <img src={Check} alt="check" className="imageCheck" />
+      </span>
+      
+      <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
+        {props.text}
+      </p>
+      
+      <span className="Icon Icon-delete"
+      onClick={props.onDelete}
+      >
+        <img src={Trash} alt="trash" className="imageTrash"/>
+      </span>
+    
+    </li>
+  );
+}
+
+export { TodoItem };
